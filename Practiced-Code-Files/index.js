5def96686a7041edb7d2f317d8906fbc8f3b10cf
duraig <duraig@gislen.com>
2025-08-08 19:11:17 +0530

    fix : js assessment 3 day

--- Practiced-Code-Files/index.js
+++ Practiced-Code-Files/index.js
@@ -1,3 +1,5 @@
+
+// Find long text using function and argument object
 function find_long()
 {
     let long_text="";
@@ -14,3 +16,16 @@ function find_long()
 const a=find_long("a","ab","abc","abcd");
 
 console.log(a);
+
+
+// List creation using function with argument object
+function list(a)
+{
+    let total=`<${a}l><li>`;
+    const arr=Array.from(arguments).slice(1);
+    total+=arr.join("</li><li>");
+    total+=`</li><${a}l>`;
+    return total;
+}
+
+list("u", "One", "Two", "Three");
